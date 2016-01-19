3adb1348b7e81c10d2b9b5e36bd056b3a28a39c1
zhanglei13 <zhanglei13@otcaix.iscas.ac.cn>
2016-01-19 09:50:02 +0800

    service

--- zeppelin-server/src/main/java/org/apache/zeppelin/modules/Module.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/modules/Module.java
@@ -1,7 +1,32 @@
 package org.apache.zeppelin.modules;
 
+import org.apache.zeppelin.udf.UDF;
+
+import java.util.Map;
+
 /**
  * Created by zhanglei on 2016/1/13.
  */
-public class Module {
+public abstract class Module {
+    private UDF udf;
+    private ModuleType type;
+    private Map<String, Object> config;
+    private String name;
+    private String description;
+    private ModuleData in;
+    private ModuleData out;
+
+    public Module() {
+    }
+
+    public abstract ModuleData execute(Map<String, Object> config);
+
+    public abstract void initConf();
+
+    public String getConf(){
+        return null;
+    }
+
+    public void registerModule(){
+    }
 }
