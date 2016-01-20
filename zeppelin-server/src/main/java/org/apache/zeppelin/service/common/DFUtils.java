d8824ad4d0f46e9d8f19e01089243276828ab2a2
zhanglei13 <zhanglei13@otcaix.iscas.ac.cn>
2016-01-20 10:48:28 +0800

    module

--- zeppelin-server/src/main/java/org/apache/zeppelin/service/common/DFUtils.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/service/common/DFUtils.java
@@ -1,7 +1,30 @@
 package org.apache.zeppelin.service.common;
 
+import java.util.List;
+
 /**
  * Created by zhanglei on 2016/1/19.
  */
 public class DFUtils {
+    private static long id = 0;
+    private List<String> savedDF;
+    private List<String> historyDF;
+
+    public long randomID() {
+        return id++;
+    }
+
+    public String createDF() {
+        String id = randomID()+"_DF";
+        historyDF.add(id);
+        return id;
+    }
+
+    public void getDFData(String id) {
+
+    }
+
+    public void getDFData() {
+
+    }
 }
