e2cbf757d3805d002791b726bfe8d13e85719e96
cuiguangfan <736068048@qq.com>
2016-01-22 17:03:12 +0800

    Merge branch 'master' of https://github.com/zhanglei13/zeppelin
    # Conflicts:
    #	zeppelin-client/src/index.html
    #	zeppelin-client/src/js/common.js
    
    updata

--- zeppelin-client/src/js/common.js
+++ zeppelin-client/src/js/common.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
             $('#modules-nav').append(content);
             $("a[id^=module_]").click(function(){
                 //first empty children under the form
-                $("#modal-container-502747-form").empty();
+                //$("#modal-container-502747-form").empty();
                 //console.log("http://133.133.133.53:8080/api/module/"+$(this).attr("type")+";name="+$(this).attr("name"));
                 $.ajax({
                     type : "get",//请求方式
@@ -35,14 +35,14 @@ $(document).ready(function(){
                         var message=data.message;
                         var bodyMap=data.body;
                         $.each(bodyMap,function(key,value){
-                            var innerStr=' <input type="text" class="form-control" id="'+key+'" placeholder="'+value+'">';
+                            var innerStr=' <input type="text" class="form-control"  name="'+key+'" id="'+key+'" placeholder="'+value+'">';
                             if(key=='driver')
-                                innerStr='<select class="form-control" id="'+key+'">'+
+                                innerStr='<select class="form-control"  name="'+key+'" id="'+key+'">'+
                                     '<option>mysql</option>'+
                                     '<option>oracle</option>'+
                                     '</select>'
                             if(key=='password')
-                                innerStr=' <input type="password" class="form-control" id="'+key+'" placeholder="'+value+'">';
+                                innerStr=' <input type="password" class="form-control"  name="'+key+'" id="'+key+'" placeholder="'+value+'">';
                             var str='<div class="form-group">'+
                                 '<label for="'+key+'" class="col-sm-2 control-label">'+value+'</label>'+
                                 ' <div class="col-sm-10">'+
