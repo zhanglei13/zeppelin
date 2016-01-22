59bc68a5961b187fd87b984041752a21dd4b1ed7
zhanglei13 <zhanglei13@otcaix.iscas.ac.cn>
2016-01-22 15:45:58 +0800

    change

--- zeppelin-client/src/js/common.js
+++ zeppelin-client/src/js/common.js
@@ -1,20 +1,26 @@
 /**
  * Created by zhanglei on 2016/1/12.
  */
-var s1,s2;
+
 $(document).ready(function(){
     // 组件导航栏加载
     $.ajax({
-        url: "http://133.133.133.53:8080/api/module/modules",
+        url: "http://133.133.133.53:8080/api/module",
         type: "GET",
         dataType:"json",
         success:function(data){
-            $.each(data.body,function(key,infos) {
-                $('#modules-nav').append("<li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+key+"<strong class=\"caret\"></strong></a>");
-                $('#modules-nav').append("<ul class=\"dropdown-menu\">");
-                appendModules(module);
-                $('#modules-nav').append("</ul></li>");
+            var content = "";
+            $.each(data.body,function(key,moduleList) {
+                content += "<li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+key+"<strong class=\"caret\"></strong></a>";
+                content += "<ul class=\"dropdown-menu\">";
+
+                $.each(moduleList, function(i, item) {
+                    content += "<li><a>"+item.nameCN+"</a></li>";
+                });
+
+                content += "</ul></li>";
             });
+            $('#modules-nav').append(content);
         }
     });
 
